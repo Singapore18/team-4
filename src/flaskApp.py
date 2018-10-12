f05370737e061a4a13fd89297c6167fa0eec08d3
herringblue <ivanckj96@gmail.com>
2018-10-12 10:59:20 +0800

    defined methods

--- src/flaskApp.py
+++ src/flaskApp.py
@@ -4,7 +4,17 @@ from flask.ext.mysql import MySQL
 mysql = MySQL()
 app = Flask(__name__)
 
-app.config['MYSQL_DATABASE_USER'] = 'jay'
 mysql.init_app(app)
 
-@app.route('/')
+@app.route('/students', methods=['POST'])
+def getAllStudents():
+
+@app.route('/questions', methods=['POST'])
+def getAllQuestions():
+
+@app.route('/survey', methods=['POST'])
+def getAllSurveys():
+
+@app.route('/results', methods=['POST'])
+def getAllResults():
+
