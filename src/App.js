0b44d5dc73123c836727189d9702f3ce67cb7488
Darryl Leong <darryleong95@gmail.com>
2018-10-12 14:14:14 +0800

    Routing Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import StudentDataComponent  from './StudentData'
+import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Login from './login'
-
 import QuestionComponent from './Question'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <StudentDataComponent />
+      <Switch>
+        <Route exact path='/' component={Login}/>
+        <Route path='/survey' component={QuestionComponent}/>
+      </Switch>
       </div>
     );
   }
