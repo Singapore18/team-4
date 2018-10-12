de49f483a18db670295d63498203bbf783717fdc
kexiaowen <e0032602@u.nus.edu>
2018-10-12 11:36:35 +0800

    add Keystone and Metric tables

--- src/backend/models.py
+++ src/backend/models.py
@@ -14,9 +14,21 @@ class QuestionBank(db.Model):
     rev = db.Column(db.Boolean)
 
 
+class Keystone(db.Model):
+    kid = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(20))
 
 
-    def  
+class Metric(db.Model):
+    mid = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(20))
+    kid = db.Column(db.Integer)
+
+
+
+class Student(db.Model):
+    nric = db.Column(db.String(4), primary_key=True)
+
     
 
     id = db.Column(db.Integer, primary_key=True)
