3be35d904a69f068b18d3b8aa6c1b63f640077f9
Jaclyn LAM Hui Yun <jaclyn.lam.2016@sis.smu.edu.sg>
2018-10-12 15:30:37 +0800

    response

--- src/backend/flaskApp.py
+++ src/backend/flaskApp.py
@@ -5,6 +5,7 @@ from models import Keystone
 from models import QuestionBank
 from models import Metric
 from models import Student
+from models import Response
 # from models import Survey
 
 from app import db, app
@@ -28,11 +29,32 @@ def getAllStudents():
 
 @app.route('/questions', methods=['GET'])
 def getAllQuestions():
-    # questions = QuestionBank.query.all()
     return jsonify(json_list=[i.serialize for i in QuestionBank.query.all()])
-    # print(QuestionBank.query.all())
-    # return "hi"
-    # return json.dumps(QuestionBank.query.all())
+   
+@app.route('/response', methods=['POST'])
+def postAnswer():
+    # print(dir(request))
+    data = request.get_data().decode('utf8')
+    print(data)
+    
+    # for response in data["response"]:
+    #     print(response)
+    #     qid = data["qid"]
+    #     sid = data["sid"]
+    #     value = data["value"]
+    #     kid = data["kid"]
+    #     mid = data["mid"]
+    #     db.session.add(Response(qid=qid, sid=sid, value=value, kid=kid, mid=mid))
+    # db.session.commit()
+    
+    return jsonify(
+        status = 200
+    )
+
+
+@app.route('/response', methods=['GET'])
+def getAnswer():
+    return jsonify(json_list=[i.serialize for i in Response.query.all()])
 
 @app.route('/postResults', methods=['POST'])
 def postResults():
@@ -40,9 +62,9 @@ def postResults():
     #     _results = request.form['results']
     pass
 
-@app.route('/questions', methods=['POST'])
-def setResults():
-    return json.dumps(QuestionBank.query.all())
+# @app.route('/questions', methods=['POST'])
+# def setResults():
+#     return json.dumps(QuestionBank.query.all())
 
 # @app.route('/survey', methods=['GET'])
 # def getAllSurveys():
@@ -56,9 +78,9 @@ def setResults():
 def getAllKeystone():
     return jsonify(json_list=[i.serialize for i in Keystone.query.all()])
 
-@app.route('/results', methods=['GET'])
-def getAllResults():
-    return Result.query.all()
+# @app.route('/results', methods=['GET'])
+# def getAllResults():
+#     return Result.query.all()
 
 
 
