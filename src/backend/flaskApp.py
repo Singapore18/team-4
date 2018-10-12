31d17436a497b9a5fb73dfcafc627149c3807f4f
Jaclyn LAM Hui Yun <jaclyn.lam.2016@sis.smu.edu.sg>
2018-10-12 14:09:45 +0800

    merged

--- src/backend/flaskApp.py
+++ src/backend/flaskApp.py
@@ -40,18 +40,9 @@ def postResults():
     # try:
     #     _results = request.form['results']
 
-<<<<<<< HEAD
 @app.route('/questions', methods=['POST'])
 def setResults():
     return json.dumps(QuestionBank.query.all())
-=======
-    #     if _results:
-    #         newResults = Result(result = _results)
-    #         db.session.add(newResults)
-    #         db.session.commit()
-    # except:
-    #     pass
->>>>>>> 134956ce7e3033ce3f650ea22de025431c5ab141
 
 # @app.route('/survey', methods=['GET'])
 # def getAllSurveys():
@@ -61,15 +52,9 @@ def setResults():
 # def getAllMetrics():
 #     return Survey.query.all()
 
-<<<<<<< HEAD
-# @app.route('/survey', methods=['GET'])
-# def getAllKeystone():
-#     return Keystone.query.all()
-=======
 @app.route('/keystone', methods=['GET'])
 def getAllKeystone():
     return Keystone.query.all()
->>>>>>> 134956ce7e3033ce3f650ea22de025431c5ab141
 
 @app.route('/results', methods=['GET'])
 def getAllResults():
