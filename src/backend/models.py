0fabb5c7da76a66e1c7a9a94426dde852dc96ffa
kexiaowen <e0032602@u.nus.edu>
2018-10-12 11:37:39 +0800

    remove unrelated parts

--- src/backend/models.py
+++ src/backend/models.py
@@ -30,11 +30,3 @@ class Student(db.Model):
     nric = db.Column(db.String(4), primary_key=True)
 
     
-
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
